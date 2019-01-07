38ebecfda1147b73d7cf497c57203a1c5f8e64f3
xc5012 <xc5012@gmail.com>
2019-01-07 07:50:10 -0500

    test2

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def hello_world():
-    return 'This is my github repo'
+    return 'This the Mission16 v2'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
